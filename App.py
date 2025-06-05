0665b9898f87764a1a72e6373bc59251c3202f20
tuankiet-1 <22dh111823@st.huflit.edu.vn>
2025-06-05 14:41:37 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask on Render!"
+    return "Chạy Flask bằng python app.py!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+   
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
