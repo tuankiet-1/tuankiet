acecc3aab021e018505335270ef489be82c23286
tuankiet-1 <22dh111823@st.huflit.edu.vn>
2025-06-05 19:35:31 +0700

    Update App.py

--- App.py
+++ App.py
@@ -8,6 +8,7 @@ def home():
     return "Chạy Flask bằng python app.py!"
 
 if __name__ == '__main__':
-   
+    # Lấy port từ biến môi trường (Render sẽ set), mặc định là 5000 nếu chạy local
     port = int(os.environ.get("PORT", 5000))
+    # host='0.0.0.0' để Flask lắng nghe mọi IP (bắt buộc với Render)
     app.run(host='0.0.0.0', port=port, debug=True)
