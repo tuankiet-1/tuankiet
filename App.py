c27f14ae109afefa546721d1140e9d3bfc6d72a9
tuankiet-1 <22dh111823@st.huflit.edu.vn>
2025-06-10 13:25:55 +0700

    Update App.py

--- App.py
+++ App.py
@@ -5,10 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Chạy Flask bằng python app.py!"
+    return "Flask chạy được trên Render!"
 
 if __name__ == '__main__':
-    # Lấy port từ biến môi trường (Render sẽ set), mặc định là 5000 nếu chạy local
-    port = int(os.environ.get("PORT", 5000))
-    # host='0.0.0.0' để Flask lắng nghe mọi IP (bắt buộc với Render)
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
